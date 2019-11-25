cd755296c9812a04b730d66589baa7e3816050e1
dewcked <rkdcksdn1234@gmail.com>
2019-11-25 21:54:10 +0900

    finished guideline

--- Assets/Scripts/LineHolder.cs
+++ Assets/Scripts/LineHolder.cs
@@ -13,39 +13,38 @@ public class LineHolder : MonoBehaviour {
 	}
     void DrawGuideLine()
     {
-        for (int i = 0; i < GameVariable.Rows; i++)
-            for (int j = 0; j < GameVariable.ColumnsA; j++)
+        for (int i = 0; i <= GameVariable.Rows; i++)
+            for (int j = 0; j <= GameVariable.ColumnsA; j++)
             {
                 Debug.Log("not that easy");
                 GameObject newline = Instantiate(LineGeneratorPrefab);
                 LineRenderer lRend = newline.GetComponent<LineRenderer>();
 
                 lRend.SetPosition(0, new Vector3(j - 0.5f, i - 0.5f, 0 - 0.5f));
-                lRend.SetPosition(1, new Vector3(i - 0.5f, j - 0.5f, GameVariable.ColumnsB - 0.5f));
+                lRend.SetPosition(1, new Vector3(j - 0.5f, i - 0.5f, GameVariable.ColumnsB - 0.5f));
+                lRend.SetPosition(0, new Vector3(j - 0.5f, i - 0.5f, 0 - 0.5f));
 
-                Destroy(newline, 5);
             }
-        for (int i = 0; i < GameVariable.ColumnsB; i++)
-            for (int j = 0; j < GameVariable.ColumnsA; j++)
+        for (int i = 0; i <= GameVariable.ColumnsB; i++)
+            for (int j = 0; j <= GameVariable.ColumnsA; j++)
             {
                 GameObject newline = Instantiate(LineGeneratorPrefab);
                 LineRenderer lRend = newline.GetComponent<LineRenderer>();
 
                 lRend.SetPosition(0, new Vector3(j - 0.5f, 0 - 0.5f, i - 0.5f));
                 lRend.SetPosition(1, new Vector3(j - 0.5f, GameVariable.Rows - 0.5f, i - 0.5f));
+                lRend.SetPosition(0, new Vector3(j - 0.5f, 0 - 0.5f, i - 0.5f));
 
-                Destroy(newline, 5);
             }
-        for (int i = 0; i < GameVariable.Rows; i++)
-            for (int j = 0; j < GameVariable.ColumnsB; j++)
+        for (int i = 0; i <= GameVariable.Rows; i++)
+            for (int j = 0; j <= GameVariable.ColumnsB; j++)
             {
                 GameObject newline = Instantiate(LineGeneratorPrefab);
                 LineRenderer lRend = newline.GetComponent<LineRenderer>();
 
                 lRend.SetPosition(0, new Vector3(0 - 0.5f, i - 0.5f, j - 0.5f));
                 lRend.SetPosition(1, new Vector3(GameVariable.ColumnsA - 0.5f, i - 0.5f, j - 0.5f));
-
-                Destroy(newline, 5);
+                lRend.SetPosition(0, new Vector3(0 - 0.5f, i - 0.5f, j - 0.5f));
             }
     }
     // Update is called once per frame
