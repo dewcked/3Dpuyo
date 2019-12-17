9c4b7f9577591a5574a4ea9d31159ed3fec5ec5b
2JeongYun <47492788+2JeongYun@users.noreply.github.com>
2019-12-17 11:54:42 +0900

    조명,기둥,그림자,바닥 추가

--- Assets/Scripts/Game/UI.cs
+++ Assets/Scripts/Game/UI.cs
@@ -6,18 +6,24 @@ public class UI : MonoBehaviour {
 
 
     [SerializeField]
-    private GameObject uiPrefabs;
+    private GameObject pillarPrefab;
+    [SerializeField]
+    private GameObject bottomPrefab;
 
     private GameObject[] pillars = new GameObject[4];
+    private GameObject bottom;
     // Use this for initialization
     void Start () {
-        pillars[0] = Instantiate(uiPrefabs, new Vector3(-0.5f, 0f, -0.5f), Quaternion.identity) as GameObject;
-        pillars[1] = Instantiate(uiPrefabs, new Vector3(2.5f, 0f, -0.5f), Quaternion.identity) as GameObject;
-        pillars[2] = Instantiate(uiPrefabs, new Vector3(2.5f, 0f, 2.5f), Quaternion.identity) as GameObject;
-        pillars[3] = Instantiate(uiPrefabs, new Vector3(-0.5f, 0f, 2.5f), Quaternion.identity) as GameObject;
+        pillars[0] = Instantiate(pillarPrefab, new Vector3(-0.5f, -0.2f, -0.5f), Quaternion.identity) as GameObject;
+        pillars[1] = Instantiate(pillarPrefab, new Vector3(2.5f, -0.2f, -0.5f), Quaternion.identity) as GameObject;
+        pillars[2] = Instantiate(pillarPrefab, new Vector3(2.5f, -0.2f, 2.5f), Quaternion.identity) as GameObject;
+        pillars[3] = Instantiate(pillarPrefab, new Vector3(-0.5f, -0.2f, 2.5f), Quaternion.identity) as GameObject;
         for (int i = 0; i < pillars.Length; i++) {
             pillars[i].transform.localScale = new Vector3(0.02f, 0.04f, 0.02f);
         }
+
+        bottom = Instantiate(bottomPrefab, new Vector3(-1.5f, -0.3f, -1.5f), Quaternion.identity) as GameObject;
+        bottom.transform.localScale = new Vector3(125f, 125f, 125f);
     }
 	
 	// Update is called once per frame
